be8ffbde2850eb7efb48cba3823d014f7133df5c
ilian.kostadinov@gmail.com <ilian.kostadinov@gmail.com>
2020-03-18 13:56:52 +0200

    Worked in simple dlete file

--- learn_basic_math.py
+++ learn_basic_math.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python3
-"""Summ, subtraction numbers"""
+"""Sum, substracion numbers"""
 
 import tkinter as tk
 import random
 
-ROOT = tk.Tk()
+
+ROOT_WIN = tk.Tk()
+ROOT_WIN.title("Clear text")
 
 FIRST_NUM = random.randint(1, 10)
 SECOND_NUM = random.randint(1, 10)
-# Define numbers
-NUM1 = tk.Label(ROOT, text=FIRST_NUM)
+print("First num %d" % FIRST_NUM)
+print("Second num %d" % SECOND_NUM)
+
+NUM1 = tk.Label(ROOT_WIN, text=FIRST_NUM)
 NUM1.grid(row=0, column=0, padx=10, pady=10)
-PLUS = tk.Label(ROOT, text="+")
+PLUS = tk.Label(ROOT_WIN, text="+")
 PLUS.grid(row=0, column=1, padx=10, pady=10)
-NUM2 = tk.Label(ROOT, text=SECOND_NUM)
+NUM2 = tk.Label(ROOT_WIN, text=SECOND_NUM)
 NUM2.grid(row=0, column=2, padx=10, pady=10)
-EQUAL = tk.Label(ROOT, text="=")
+EQUAL = tk.Label(ROOT_WIN, text="=")
 EQUAL.grid(row=0, column=3, padx=10, pady=10)
-ENTRY_WIDGET = tk.Entry(ROOT)
-ENTRY_WIDGET.grid(row=0, column=4, padx=10, pady=10)
 
-def my_click():
-    """Executed when check_button pressed"""
-    RESULT = ENTRY_WIDGET.get()
-    if int(RESULT) == FIRST_NUM+SECOND_NUM:
-        ENTRY_WIDGET1 = tk.Entry(ROOT, bg="green")
-    else:
-        ENTRY_WIDGET1 = tk.Entry(ROOT, bg="red")
-    ENTRY_WIDGET1.grid(row=0, column=4, padx=10, pady=10)
-    return
 
-CHECK_BUTTON = tk.Button(ROOT, text="ПРОВЕРИ", padx=70, pady=20, command=my_click)
-CHECK_BUTTON.grid(row=0, column=5, padx=10, pady=10)
+IMP_FIELD = tk.Entry(ROOT_WIN)
+IMP_FIELD.focus_set()
+IMP_FIELD.grid(row=0, column=4)
 
-ROOT.mainloop()
+
+def check():
+    """Executed when check button pressed"""
+    num = IMP_FIELD.get()
+    if int(num) == FIRST_NUM+SECOND_NUM:
+        IMP_FIELD.configure({"background": "green"})
+        return True
+    IMP_FIELD.configure({"background": "red"})
+    IMP_FIELD.delete(0, "end")
+    return False
+
+CHECK_BUT = tk.Button(ROOT_WIN, text="ПРОВЕРИ", command=check)
+CHECK_BUT.grid(row=0, column=5)
+
+
+ROOT_WIN.mainloop()
