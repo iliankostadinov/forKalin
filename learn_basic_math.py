6fade99ffd9490faf5661345ece4c36655c4fa49
iliankostadinov <ilian.kostadinov@gmail.com>
2020-03-23 07:47:09 +0200

    Solution with class object

--- learn_basic_math.py
+++ learn_basic_math.py
@@ -4,42 +4,45 @@
 import tkinter as tk
 import random
 
+class One_Line(object):
+    ROOT_WIN = tk.Tk()
+    ROOT_WIN.title("Задачи за Калин")
 
-ROOT_WIN = tk.Tk()
-ROOT_WIN.title("Clear text")
-
-FIRST_NUM = random.randint(1, 10)
-SECOND_NUM = random.randint(1, 10)
-print("First num %d" % FIRST_NUM)
-print("Second num %d" % SECOND_NUM)
-
-NUM1 = tk.Label(ROOT_WIN, text=FIRST_NUM)
-NUM1.grid(row=0, column=0, padx=10, pady=10)
-PLUS = tk.Label(ROOT_WIN, text="+")
-PLUS.grid(row=0, column=1, padx=10, pady=10)
-NUM2 = tk.Label(ROOT_WIN, text=SECOND_NUM)
-NUM2.grid(row=0, column=2, padx=10, pady=10)
-EQUAL = tk.Label(ROOT_WIN, text="=")
-EQUAL.grid(row=0, column=3, padx=10, pady=10)
-
-
-IMP_FIELD = tk.Entry(ROOT_WIN)
-IMP_FIELD.focus_set()
-IMP_FIELD.grid(row=0, column=4)
+    def __init__(self, row_num=0):
+        self.first_num = random.randint(1, 10)
+        self.second_num = random.randint(1, 10)
+        print("First num %d" % self.first_num)
+        print("Second num %d" % self.second_num)
+        self.num1 = tk.Label(One_Line.ROOT_WIN, text=self.first_num)
+        self.num1.grid(row=row_num, column=0, padx=10, pady=10)
+        self.plus = tk.Label(One_Line.ROOT_WIN, text="+")
+        self.plus.grid(row=row_num, column=1, padx=10, pady=10)
+        self.num2 = tk.Label(One_Line.ROOT_WIN, text=self.second_num)
+        self.num2.grid(row=row_num, column=2, padx=10, pady=10)
+        self.equal = tk.Label(One_Line.ROOT_WIN, text="=")
+        self.equal.grid(row=row_num, column=3, padx=10, pady=10)
+        self.imp_field = tk.Entry(One_Line.ROOT_WIN)
+        self.imp_field.grid(row=row_num, column=4)
 
 
-def check():
-    """Executed when check button pressed"""
-    num = IMP_FIELD.get()
-    if int(num) == FIRST_NUM+SECOND_NUM:
-        IMP_FIELD.configure({"background": "green"})
-        return True
-    IMP_FIELD.configure({"background": "red"})
-    IMP_FIELD.delete(0, "end")
-    return False
 
-CHECK_BUT = tk.Button(ROOT_WIN, text="ПРОВЕРИ", command=check)
+    def check(self):
+        """Executed when check button pressed"""
+        num = self.imp_field.get()
+        if int(num) == self.first_num+self.second_num:
+            self.imp_field.configure({"background": "green"})
+            return True
+        self.imp_field.configure({"background": "red"})
+        self.imp_field.delete(0, "end")
+        return False
+
+
+first_line = One_Line(0)
+CHECK_BUT = tk.Button(One_Line.ROOT_WIN, text="ПРОВЕРИ", command=first_line.check)
 CHECK_BUT.grid(row=0, column=5)
 
+second_line =One_Line(1)
+CHECK_BUT = tk.Button(One_Line.ROOT_WIN, text="ПРОВЕРИ", command=second_line.check)
+CHECK_BUT.grid(row=1, column=5)
 
-ROOT_WIN.mainloop()
+One_Line.ROOT_WIN.mainloop()
